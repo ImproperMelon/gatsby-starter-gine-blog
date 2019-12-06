1e01bb208d44e05837afd7ec81335092075b5025
ImproperMelon <45617347+ImproperMelon@users.noreply.github.com>
2019-12-06 09:17:30 +0800

    Change the urls

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,13 @@ module.exports = {
         {
             resolve: `gatsby-theme-gine-blog`,
             options: {
-                configTable: "https://www.notion.so/b4af861710064848aaf9f859c79bb127?v=0de244d6f8414ade9b7b147adcb2f78e"
+                configTable: "https://www.notion.so/impropermelon/71cbce5658bc4b3b9e835c1c1c8870c2?v=de8fab50fe454f48a55e0f29206ced87"
             }
         },
         {
             resolve: `gatsby-source-notion-database`,
             options: {
-                configTable: "https://www.notion.so/65b35da1762e4f259904bb4cc38e54fd?v=89e80623e52e4419a35f20e54056cfb8"
+                configTable: "https://www.notion.so/impropermelon/8aa826240b1c42d9905b85e4769fda3e?v=f2e78341da144dd9b17b2eaaffbb3010"
             }
         }
     ],
